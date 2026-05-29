8e8fce306bf93c4c2c25feba1762aea1e53fc6d6
Saifullah <ipullell354@gmail.com>
2026-05-29 19:05:42 +0800

    add main.py

--- src/transform.py
+++ src/transform.py
@@ -1,22 +1,22 @@
-# from extract import fetchMovies
-# import pandas as pd
+from extract import fetchMovies
+import pandas as pd
 
-# def transformMovies(data):
-#     df = pd.DataFrame(data['results'])
+def transformMovies(data):
+    df = pd.DataFrame(data['results']) # mengubah menjadi dataframe(tabel)
 
-#     df = df [
-#         [
-#             "id",
-#             "title",
-#             "genre_ids",
-#             "popularity",
-#             "vote_average",
-#             "vote_count",
-#             "release_date"
-#         ]
-#     ]
+    df = df [ # menyaring data yg akan di ambil
+        [
+            "id",
+            "title",
+            "genre_ids",
+            "popularity",
+            "vote_average",
+            "vote_count",
+            "release_date"
+        ]
+    ]
 
-#     df["release_date"] = pd.to_datetime(df["release_date"])
-#     df["popularity"] = df["popularity"].round(1)
-#     df["vote_average"] = df["vote_average"].round(2)
-#     df_explode = df.explode("genre_ids")
+    df["release_date"] = pd.to_datetime(df["release_date"]) # mengubah tipe data menjadi datetime
+    df["popularity"] = df["popularity"].round(1) # mengubah data menjadi desimal dan mengambil 1 angka di belakang koma
+    df["vote_average"] = df["vote_average"].round(2)  # mengubah data menjadi desimal dan mengambil 2 angka di belakang koma
+    df_explode = df.explode("genre_ids") # memecah data array menjadi baris
